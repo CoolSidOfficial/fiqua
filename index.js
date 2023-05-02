a8a636cb2b543363dd6b0e95c3fdd8e6cceed16b
CoolSidOfficial <hackersiddhant1@gmail.com>
2023-05-02 17:43:15 +0530

    uploading code

---
+++ index.js
@@ -0,0 +1,8 @@
+let value=document.querySelectorAll('');
+let interval=5000;
+value.forEach(valueDisplay=>{
+let start=0;
+let endvalue=parseInt(value.getAttribute("dataval"));
+console.log(endvalue);
+
+});
